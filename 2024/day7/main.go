a682bd4f22ba975138e38e790569c07db06dfdc3
Gabriel Fioravante <61801407+gabefiori@users.noreply.github.com>
2024-12-14 00:59:40 -0300

    feat: day 7

---
+++ 2024/day7/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	_ "embed"
+	"fmt"
+	"slices"
+	"strconv"
+	"strings"
+
+	"github.com/gabefiori/aoc-go/pkg"
+)
+
+const (
+	add int = iota
+	mult
+	concat
+)
+
+//go:embed input.txt
+var input []byte
+
+func solve(sc *bufio.Scanner, ops []int) int {
+	var sum, target int
+	var part, comb, vals []int
+
+	for sc.Scan() {
+		line := sc.Text()
+		sep := strings.Index(line, ":")
+		target = pkg.ParseInt(line[:sep])
+		vals = pkg.ParseInts(strings.Split(line[sep+2:], " "), vals)
+
+		part, comb = part[:0], comb[:0]
+		operations(len(vals)-1, part, ops, &comb)
+
+		for chunk := range slices.Chunk(comb, len(vals)-1) {
+			t := vals[0]
+			for i, val := range vals[1:] {
+				switch chunk[i] {
+				case add:
+					t += val
+				case mult:
+					t *= val
+				case concat:
+					t = pkg.ParseInt(strconv.Itoa(t) + strconv.Itoa(val))
+				}
+			}
+
+			if t == target {
+				sum += t
+				break
+			}
+		}
+	}
+
+	return sum
+}
+
+func operations(n int, curr, ops []int, res *[]int) {
+	if len(curr) == n {
+		*res = append(*res, curr...)
+		return
+	}
+
+	for _, op := range ops {
+		operations(n, append(curr, op), ops, res)
+	}
+}
+
+func main() {
+	part1 := solve(pkg.NewScanner(input), []int{add, mult})
+	part2 := solve(pkg.NewScanner(input), []int{add, mult, concat})
+	fmt.Println(part1, part2)
+}
---
+++ 2024/day7/main_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"testing"
+
+	_ "embed"
+
+	"github.com/gabefiori/aoc-go/pkg"
+	"github.com/stretchr/testify/assert"
+)
+
+//go:embed input_test.txt
+var inputTest []byte
+
+func Test(t *testing.T) {
+	assert.Equal(t, 3749, solve(pkg.NewScanner(inputTest), []int{add, mult}))
+	assert.Equal(t, 11387, solve(pkg.NewScanner(inputTest), []int{add, mult, concat}))
+}
+
+func Benchmark(b *testing.B) {
+	b.Run("Part 1", func(b *testing.B) {
+		reader := bytes.NewReader(input)
+		for i := 0; i < b.N; i++ {
+			_, _ = reader.Seek(0, 0)
+			solve(bufio.NewScanner(reader), []int{add, mult})
+		}
+	})
+
+	b.Run("Part 2", func(b *testing.B) {
+		reader := bytes.NewReader(input)
+		for i := 0; i < b.N; i++ {
+			_, _ = reader.Seek(0, 0)
+			solve(bufio.NewScanner(reader), []int{add, concat, concat})
+		}
+	})
+}
